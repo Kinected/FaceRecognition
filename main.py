77b586960349d6baab8dcb717e7da9dc9ab63d4a
Antoine Maes <92756820+AntoLeRigoloo@users.noreply.github.com>
2024-04-01 22:21:06 +0200

    send websockets

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import face_recognition
 import cv2
 import numpy as np
+import asyncio
+import websockets
+from websockets.exceptions import ConnectionClosed, ConnectionClosedError
+import json
+
+uri = "ws://localhost:8000/ws/swipes"
+
+
+
+async def send_gesture(websocket, gesture: dict):
+    try:
+        # print("Sending gesture...")
+        await websocket.send(json.dumps(gesture))
+        # print("Gesture sent.")
+    except ConnectionClosedError:
+        print("WebSocket connection was closed unexpectedly.")
+
 
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
@@ -35,45 +52,41 @@ face_encodings = []
 face_names = []
 process_this_frame = True
 
-while True:
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
+async def detect_faces(frame):
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    # Only process every other frame of video to save time
-    if process_this_frame:
-        # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
 
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
+                        
+    # Find all the faces and face encodings in the current frame of video
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-        
-        # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+    face_names = []
+    for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        name = "Unknown"
 
-        face_names = []
-        for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-            name = "Unknown"
+        # # If a match was found in known_face_encodings, just use the first one.
+        # if True in matches:
+        #     first_match_index = matches.index(True)
+        #     name = known_face_names[first_match_index]
 
-            # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
+        # Or instead, use the known face with the smallest distance to the new face
+        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+        face_names.append(name)
 
-            # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
-
-            face_names.append(name)
-
-    process_this_frame = not process_this_frame
+    return face_names, face_locations   
 
 
+
+def display_image(frame, face_locations, face_names):
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
@@ -90,12 +103,46 @@ while True:
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-    # Display the resulting image
     cv2.imshow('Video', frame)
+    cv2.waitKey(1)
+    
 
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+
+async def main():
+    global process_this_frame
+    while True:
+        try:
+            async with websockets.connect(uri) as websocket:
+                while True:
+                    # Grab a single frame of video
+                    ret, frame = video_capture.read()
+
+                    # Only process every other frame of video to save time
+                    if process_this_frame:
+                        face_names, face_locations = await detect_faces(frame)
+
+                        if len(face_names) > 0:
+                            payload = {
+                                "swipe": face_names[0]
+                            }
+                            await send_gesture(websocket, payload)
+
+                    process_this_frame = not process_this_frame
+
+
+                    # Display the frame
+                    # display_image(frame, face_locations, face_names)
+
+                    
+
+        except ConnectionClosed:
+            print("Connection lost. Reconnecting...")
+            continue
+        except websockets.exceptions.InvalidStatusCode as e:
+            print(f"Server rejected WebSocket connection: {e}")
+            break
+
+asyncio.run(main())
 
 # Release handle to the webcam
 video_capture.release()
